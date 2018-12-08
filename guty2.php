5c159ff2751f2fc68af842f59ba8d48e69e66817
jschofie <jschofie@nerdery.com>
2018-12-08 15:39:15 -0600

    Initial commit

---
+++ guty2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Guty2
+ * Description: A rebuild of guty blocks
+ * Version: 0.0.1
+ * Author: Jim Schofield
+ * Text Domain: guty2
+ * Domain Path: /languages
+ *
+ * @package guty2
+ */
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
